87989d21ac0da2b3b4c84a5a1feb60948e14a474
Jan Zakrzewski <janzak214@gmail.com>
2024-07-25 03:12:36 +0200

    Changes

--- composeApp/src/androidMain/kotlin/pl/janzak/cmp_demo/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/pl/janzak/cmp_demo/MainActivity.kt
@@ -1,25 +1,56 @@
 package pl.janzak.cmp_demo
 
 import App
+import NumberScreen
+import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Window
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.VisibleForTesting
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+
+internal val DefaultLightScrim = Color.argb(0xe6, 0xFF, 0xFF, 0xFF)
+
+// The dark scrim color used in the platform.
+// https://cs.android.com/android/platform/superproject/+/master:frameworks/base/core/res/res/color/system_bar_background_semi_transparent.xml
+// https://cs.android.com/android/platform/superproject/+/master:frameworks/base/core/res/remote_color_resources_res/values/colors.xml;l=67
+@VisibleForTesting
+internal val DefaultDarkScrim = Color.argb(0x80, 0x1b, 0x1b, 0x1b)
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge(statusBarStyle =  SystemBarStyle.auto(DefaultLightScrim, DefaultDarkScrim))
         setContent {
+            val view = LocalView.current
+            SideEffect {
+                val window = (view.context as Activity).window
+                WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
+            }
             App()
         }
     }
 }
 
-@Preview
+@Preview(showSystemUi = false, name = "Preview", group = "A")
 @Composable
-fun AppAndroidPreview() {
-    App()
+fun Screen1() {
+    NumberScreen(goBack = { /*TODO*/ }, number = 42)
+}
+
+
+@Preview(showSystemUi = false, name = "Preview", group = "A")
+@Composable
+fun Screen2() {
+    NumberScreen(goBack = { /*TODO*/ }, number = 23)
 }
 
--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,4 +1,19 @@
+import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.CubicBezierEasing
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.PathEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.slideIn
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -19,6 +34,9 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.IntOffset
+import androidx.navigation.NavHostController
+import androidx.navigation.NavOptions
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
@@ -60,6 +78,15 @@ fun Test2() {
     }
 }
 
+
+object Easing {
+    val emphasizedDecelerate = CubicBezierEasing(0.05f, 0.7f, 0.1f, 1f)
+    val emphasizedAccelerate = CubicBezierEasing(0.3f, 0f, 0.8f, 0.15f)
+
+    val enterDuration = 400
+    val exitDuration = 200
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Preview
@@ -67,59 +94,135 @@ fun App() {
 
     val navController = rememberNavController()
     MaterialTheme {
+
         NavHost(navController = navController, startDestination = Route.Home) {
-            composable<Route.Home> {
-                Column(
-                    Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Row {
-                        for (i in 0..4) {
-                            Button(onClick = {
-                                navController.navigate(Route.Number(i))
-                            }) { Text("Route $i") }
-                        }
-                    }
-
-                    Test2()
-                }
-            }
-
-            composable<Route.Number> { backStackEntry ->
-                val route: Route.Number = backStackEntry.toRoute()
-
-                var showContent by remember { mutableStateOf(false) }
-
-                Scaffold(
-                    topBar = {
-                        TopAppBar(
-                            title = { Text("Hello") },
-                            navigationIcon = {
-                                IconButton(onClick = { println("Wtf"); navController.popBackStack() }) {
-                                    Icon(
-                                        Icons.AutoMirrored.Filled.ArrowBack,
-                                        "Go back"
-                                    )
-                                }
-                            },
-                            colors = TopAppBarDefaults.topAppBarColors(
-                                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                                titleContentColor = MaterialTheme.colorScheme.primary,
-                            ),
-                        )
-                    }
-                ) { padding ->
+            composable<Route.Home>(
+                enterTransition = { EnterTransition.None },
+                exitTransition = { ExitTransition.None }) {
+                Scaffold(topBar = {
+                    TopAppBar(
+                        title = { Text("Home") },
+                        colors = TopAppBarDefaults.topAppBarColors(
+                            containerColor = MaterialTheme.colorScheme.primaryContainer,
+                            titleContentColor = MaterialTheme.colorScheme.primary,
+                        ),
+                    )
+                }) { padding ->
                     Column(
                         Modifier.fillMaxWidth().padding(padding),
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        Text(
-                            "Number: ${route.value}",
-                            style = MaterialTheme.typography.displayLarge
-                        )
+                        Row {
+                            for (i in 0..4) {
+                                Button(onClick = {
+                                    navController.navigate(Route.Number(i))
+                                }) { Text("Route $i") }
+                            }
+                        }
+
                         Test2()
                     }
                 }
+            }
+
+
+
+            composable<Route.Number>(
+                enterTransition = {
+
+                    fadeIn(
+                        animationSpec = tween(
+                            durationMillis = Easing.enterDuration,
+                            easing = Easing.emphasizedDecelerate
+                        )
+                    ) + slideIntoContainer(
+                        towards = AnimatedContentTransitionScope.SlideDirection.Start,
+                        animationSpec = tween(
+                            durationMillis = Easing.enterDuration,
+                            easing = Easing.emphasizedDecelerate
+                        )
+                    )
+                },
+                exitTransition = {
+                    fadeOut(
+                        animationSpec = tween(
+                            durationMillis = Easing.exitDuration,
+                            easing = Easing.emphasizedAccelerate
+                        )
+                    ) + slideOutOfContainer(
+                        towards = AnimatedContentTransitionScope.SlideDirection.End,
+                        animationSpec = tween(
+                            durationMillis = Easing.exitDuration,
+                            easing = Easing.emphasizedAccelerate
+                        )
+                    )
+                },
+            ) { backStackEntry ->
+                val route: Route.Number = backStackEntry.toRoute()
+                NumberScreen({ navController.popBackStack() }, route.value)
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NumberScreen(goBack: () -> Unit, number: Int) {
+    val animationState =
+        remember { MutableTransitionState(false).apply { targetState = true } }
+    val stateName = when {
+        animationState.isIdle && animationState.currentState -> "Visible"
+        !animationState.isIdle && animationState.currentState -> "Disappearing"
+        animationState.isIdle && !animationState.currentState -> "Invisible"
+        else -> "Appearing"
+    }
+
+    AnimatedVisibility(
+        visibleState = animationState,
+        enter = EnterTransition.None,
+        exit = ExitTransition.None
+    ) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("Hello $stateName") },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            if (animationState.isIdle && animationState.currentState) {
+                                println("Wtf"); goBack()
+                            }
+                        }) {
+                            Icon(
+                                Icons.AutoMirrored.Filled.ArrowBack,
+                                "Go back"
+                            )
+                        }
+                    },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        titleContentColor = MaterialTheme.colorScheme.primary,
+                    ),
+                )
+            }
+        ) { padding ->
+
+            Column(
+                Modifier.fillMaxWidth().padding(padding),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    "Number: $number",
+                    style = MaterialTheme.typography.displayLarge,
+                    modifier = Modifier.animateEnterExit(
+                        enter = fadeIn(
+                            tween(
+                                durationMillis = Easing.enterDuration,
+                                easing = Easing.emphasizedDecelerate
+                            )
+                        )
+                    )
+                )
+                Test2()
             }
         }
     }
