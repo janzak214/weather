b37303a0dedbfd71918564ef2f197cc9c8b5661d
Jan Zakrzewski <janzak214@gmail.com>
2024-07-26 19:16:33 +0200

    Update HomeScreen

--- composeApp/src/commonMain/kotlin/ui/screens/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/screens/HomeScreen.kt
@@ -1,43 +1,102 @@
 package ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navigateNumber: (number: Int) -> Unit) {
     Scaffold(topBar = {
         CenterAlignedTopAppBar(
-            title = { Text("Home") },
-            colors = TopAppBarDefaults.topAppBarColors(
-                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                titleContentColor = MaterialTheme.colorScheme.primary,
-            ),
+            title = { Text("Counters") },
+            colors = TopAppBarDefaults.topAppBarColors(),
         )
-    }) { padding ->
-        Column(
-            Modifier.fillMaxWidth().padding(padding),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Row {
-                for (i in 0..4) {
-                    Button(onClick = {
-                        navigateNumber(i)
-                    }) { Text("Route $i") }
-                }
+    },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {},
+                containerColor = MaterialTheme.colorScheme.tertiaryContainer,
+                contentColor = MaterialTheme.colorScheme.onTertiaryContainer,
+            ) {
+                Icon(
+                    Icons.Default.Add,
+                    "Create a new counter"
+                )
             }
+        }
+    ) { padding ->
+        val counters = remember { (1..10).toList() }
+
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(minSize = 128.dp),
+            modifier = Modifier.padding(padding).padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(counters) {
+                CounterCard(
+                    "Card $it",
+                    value = it,
+                    onClick = { navigateNumber(it) },
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun CounterCard(name: String, value: Int, onClick: () -> Unit, modifier: Modifier = Modifier) {
+    Card(
+        onClick = onClick,
+        colors = CardDefaults.elevatedCardColors()
+            .copy(
+                containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            ),
+        elevation = CardDefaults.elevatedCardElevation(),
+        modifier = modifier
+    ) {
+        Column(modifier = Modifier.padding(8.dp).fillMaxSize()) {
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                "$value",
+                style = MaterialTheme.typography.displayMedium,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.align(Alignment.CenterHorizontally).alpha(0.7f)
+                    .padding(vertical = 4.dp)
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                name,
+                style = MaterialTheme.typography.titleSmall.copy(
+                ),
+            )
+
         }
     }
 }
