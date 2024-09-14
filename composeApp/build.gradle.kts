692c421267ec6289992c91cc6988ff181860d97d
Jan Zakrzewski <janzak214@users.noreply.github.com>
2024-09-14 20:08:04 +0200

    Update build.gradle.kts

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -127,7 +127,7 @@ android {
         getByName("release") {
             isMinifyEnabled = false
             signingConfig = signingConfigs.getByName("debug")
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles("proguard-rules.pro")
         }
     }
     compileOptions {
