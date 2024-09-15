f0c9eb2b4192d9dcf7bfb2a0357768062554fe3f
Jan Zakrzewski <janzak214@gmail.com>
2024-09-15 14:01:27 +0200

    Update app icon and splash screen

--- composeApp/src/desktopMain/kotlin/pl/janzak/weather/data/Database.desktop.kt
+++ composeApp/src/desktopMain/kotlin/pl/janzak/weather/data/Database.desktop.kt
@@ -2,13 +2,23 @@ package pl.janzak.weather.data
 
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
+import net.harawata.appdirs.AppDirsFactory
 import pl.janzak.weather.database.Database
+import java.nio.file.Files
 import java.util.Properties
+import kotlin.io.path.Path
 
 actual class DriverFactory {
     actual fun createDriver(): SqlDriver {
+        val dataDirectory = AppDirsFactory.getInstance()
+            .getUserDataDir("pl.janzak.weather", null, null)
+        requireNotNull(dataDirectory)
+        val directoryPath = Path(dataDirectory)
+        Files.createDirectories(directoryPath)
+        val dbPath = directoryPath.resolve("database.db")
+
         val driver: SqlDriver = JdbcSqliteDriver(
-            url = "jdbc:sqlite:database.db",
+            url = "jdbc:sqlite:$dbPath",
             properties = Properties(),
             schema = Database.Schema
         )
--- composeApp/src/desktopMain/kotlin/pl/janzak/weather/main.kt
+++ composeApp/src/desktopMain/kotlin/pl/janzak/weather/main.kt
@@ -1,23 +1,19 @@
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import org.jetbrains.compose.resources.stringResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.jetbrains.compose.resources.vectorResource
 import resources.Res
 import resources.app_name
+import resources.icon_rounded
 
 fun main() = application {
+    val painter = rememberVectorPainter(vectorResource(Res.drawable.icon_rounded))
     Window(
         onCloseRequest = ::exitApplication,
         title = stringResource(Res.string.app_name),
+        icon = painter,
     ) {
         App()
     }
-}
-
-
-@Preview
-@Composable
-fun Test() {
-    App()
 }
